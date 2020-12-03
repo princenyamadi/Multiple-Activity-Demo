9a03e75bbd57f4fe31037c328936768451d95229
princenyamadi <princenyamadi.pn@gmail.com>
2020-12-03 17:46:00 +0000

    Passing data from list to another activity

--- app/src/main/java/tech/peny/multipleactivitydemo/MainActivity.java
+++ app/src/main/java/tech/peny/multipleactivitydemo/MainActivity.java
@@ -5,16 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    public void goToNext(View view){
-        Intent intent = new Intent(getApplicationContext(),SecondActivity.class);
-        intent.putExtra("age",28);
-        startActivity(intent);
-    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+        ArrayList<String> friends = new ArrayList<String>();
+
+        friends.add("Fido");
+        friends.add("Sean");
+        friends.add("Phoebe");
+        friends.add("Irene");
+        friends.add("Michelle");
+        friends.add("Stephanie");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,friends);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(),SecondActivity.class);
+                intent.putExtra("name", friends.get(position));
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/tech/peny/multipleactivitydemo/SecondActivity.java
+++ app/src/main/java/tech/peny/multipleactivitydemo/SecondActivity.java
@@ -8,22 +8,17 @@ import android.view.View;
 import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
-    public void goBack(View view){
-//        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-//
-//        startActivity(intent);
-        finish();
 
 
-    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
         Intent intent = getIntent();
-        int age = intent.getIntExtra("age",0);
+        String name = intent.getStringExtra("name");
 
-        Toast.makeText(this,Integer.toString(age),Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,name,Toast.LENGTH_SHORT).show();
     }
 }
