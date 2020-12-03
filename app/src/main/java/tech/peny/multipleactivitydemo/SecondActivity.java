9c4e732a3434b0e37fd35dfddfefb617380955f7
princenyamadi <princenyamadi.pn@gmail.com>
2020-12-03 17:25:33 +0000

    Moving from screen to screen

--- app/src/main/java/tech/peny/multipleactivitydemo/SecondActivity.java
+++ app/src/main/java/tech/peny/multipleactivitydemo/SecondActivity.java
@@ -6,15 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-    public void goToNext(View view){
-        Intent intent = new Intent(getApplicationContext(),SecondActivity.class);
+public class SecondActivity extends AppCompatActivity {
+    public void goBack(View view){
+//        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+//
+//        startActivity(intent);
+        finish();
 
-        startActivity(intent);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
     }
 }
